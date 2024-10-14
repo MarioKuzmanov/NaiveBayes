b1f7b360444dd26abd9ec0f327489408654172ed
MarioKuzmanov <googlemario1@abv.bg>
2024-10-14 15:02:27 +0200

    NaiveBayesModel

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# NaiveBayes
+<h1 align="center"> Naive Bayes for Text Classification </h1>
+
+<em>Optimized for Sentiment Analysis</em>
+
+<h4> N.B. The idea is to showcase the model implementation and its' usage. </h4>
---
+++ preprocessor_large_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+class Processor(object):
+    def __init__(self):
+        label2sentiment = {"__label__1": "negative", "__label__2": "positive"}
+        labels, texts = [], []
+        length = len("__label__1")
+        with open("large_data/train.ft.txt", "rt", encoding="utf8") as f:
+            for line in f.readlines():
+                labels.append(label2sentiment[line[: length].strip()])
+                texts.append(line[length:].strip())
+        assert len(labels) == len(texts)
+
+        labels, texts = np.array(labels), np.array(texts)
+
+        labels = np.random.choice(labels, 30000)
+        texts = np.random.choice(texts, 30000)
+
+        self.df = pd.DataFrame({"sentiment": labels, "text": texts})
+
+        print("Dataset Build!")
